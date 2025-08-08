26bb20d6ae13246e375da906dc902d4ce64f9a76
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-08 17:11:32 +0600

    [Feature] Location Preview cards with asymmetric Transitions

--- SwiftfulMapApp/Models/Location.swift
+++ SwiftfulMapApp/Models/Location.swift
@@ -8,7 +8,8 @@
 import Foundation
 import MapKit
 
-struct Location: Identifiable {
+struct Location: Identifiable, Equatable {
+   
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
@@ -22,6 +23,11 @@ struct Location: Identifiable {
         // cityName = "Rome"
         // id = "ColosseumRome"
         name + cityName
+    }
+    
+    // Equatable
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
     }
 }
 
--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -19,6 +19,26 @@ struct LocationsView: View {
         ZStack {
             Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+                
+                ZStack {
+                    ForEach(vm.locations) { location in
+                        if(vm.mapLocation == location) {
+                            LocationPreviewView(location: location)
+                                .shadow(color: Color.black.opacity(0.3), radius: 20)
+                                .padding()
+                                .transition(.asymmetric(
+                                    insertion: .move(edge: .trailing),
+                                    removal: .move(edge: .leading)))
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -26,4 +46,35 @@ struct LocationsView: View {
 #Preview {
     LocationsView()
         .environmentObject(LocationsViewModel())
+}
+
+
+extension LocationsView {
+    private var header: some View {
+        VStack {
+            Button(action: vm.toggleLocationsList) {
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundStyle(Color.primary)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .animation(.none, value: vm.mapLocation)
+                    .overlay(alignment: .leading) {
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundStyle(Color.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationsList ? 180 : 0))
+                    }
+            }
+            
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
 }
