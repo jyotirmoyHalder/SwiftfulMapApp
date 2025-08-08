32b35306a735da634540c8a3106d89e9a3fa1c4d
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-08-08 10:22:19 +0600

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by jyotirmoy_halder on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
